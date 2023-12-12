324e9c5407f38d7f9c0258c84bb97b352f574c17
Nichole D Berry <131027631+Lyran-create@users.noreply.github.com>
2023-12-11 22:36:23 -0500

    Add files via upload
    Original files and accommodated or changed files for updates. Feel free. :D

---
+++ Card.java
@@ -0,0 +1,17 @@
+public class Card {
+    private String suit;
+    private String rank;
+
+    public Card(String suit, String rank) {
+        // Implementation of Card constructor
+    }
+
+    public int getValue() {
+        // Implementation of getting the card value
+    }
+
+    @Override
+    public String toString() {
+        // Implementation of converting the card to a string
+    }
+}
---
+++ Dealer.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer {
+    private List<Card> hand;
+
+    public Dealer() {
+        // Implementation of Dealer constructor
+    }
+
+    public void resetHand() {
+        // Implementation of resetting the hand
+    }
+
+    public void addCard(Card card) {
+        // Implementation of adding a card to the hand
+    }
+
+    public boolean shouldHit() {
+        // Implementation of determining if the dealer should hit
+    }
+
+    public int calculateHandValue() {
+        // Implementation of calculating the hand value
+    }
+
+    public void displayHand() {
+        // Implementation of displaying the hand
+    }
+}
